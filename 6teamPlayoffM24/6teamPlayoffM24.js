f3912ce9391ec00b6dfb0137095dbd01bc013531
Aashu Krishnan <gpstore31@gmail.com>
2024-02-03 13:54:27 -0600

    Updated 6 team playoff tool to use FranchiseUtils

--- 6teamPlayoffM24/6teamPlayoffM24.js
+++ 6teamPlayoffM24/6teamPlayoffM24.js
@@ -1,65 +1,12 @@
-
 const fs = require('fs');
 const prompt = require('prompt-sync')();
-const path = require('path');
-const os = require('os');
 const Franchise = require('madden-franchise');
+const FranchiseUtils = require('../lookupFunctions/FranchiseUtils');
 
+console.log("This program will update your Madden 24 franchise file to use the 6 team playoff format. This tool must be run during wildcard week.\n")
+const gameYear = '24';
 
-console.log("This program will update your Madden 24 franchise file to use the 6 team playoff format. This tool must be run during wildcard week.")
-var gamePrompt = '24';
-
-const documentsDir = path.join(os.homedir(), `Documents\\Madden NFL ${gamePrompt}\\saves\\`); //Two paths - One using default, one with OneDrive
-const oneDriveDir = path.join(os.homedir(),`OneDrive\\Documents\\Madden NFL ${gamePrompt}\\saves\\`)
-default_path = documentsDir // Set to default dir first
-
-if (fs.existsSync(documentsDir)) { //First, check if our Madden saves are in the default location
-  default_path = documentsDir
-}
-else if (fs.existsSync(oneDriveDir)) {
-  default_path = oneDriveDir
-}
-else {
-  console.log(`IMPORTANT! Couldn't find the path to your Madden ${gamePrompt} save files. When selecting your file, make sure to give the FULL PATH.`)
-}
-
-//Our franchise file
-function selectFranchiseFile(franchise,default_path) {
-  while (franchise == "") { //While we haven't selected a franchise file...
-    try {
-      console.log("Please enter the name of your franchise file. Either give the full path of the file OR just give the file name (ie CAREER-BEARS) if it's in your documents folder. ")
-      var fileName = prompt()
-      if (fileName.startsWith("CAREER-")) {
-        let fullPath = default_path.concat(fileName);
-        var franchise = new Franchise(fullPath);
-        return franchise;
-
-      }
-      else {
-        var search = '/'
-        var replacer = new RegExp(search, 'g')
-        var fileName = fileName.replace(replacer,'\\')
-        var franchise = new Franchise(fileName);
-        return franchise;
-      }
-
-    }
-    catch (e) {
-      console.log(e)
-      console.log("Invalid franchise file name/path given. Please try again.")
-      continue
-
-    }
-
-  }
-}
-
-async function bin2Dec(binary) {
-  return parseInt(binary, 2);
-};
-
-var franchise = "";
-franchise = selectFranchiseFile(franchise,default_path)
+const franchise = FranchiseUtils.selectFranchiseFile(gameYear);
 
 franchise.on('ready', async function () {
     const teamTable = franchise.getTableByUniqueId(502886486);
@@ -102,7 +49,7 @@ franchise.on('ready', async function () {
 		{
 			let homeTeamBinVal = seasonGameTable.records[j]['HomeTeam'];
 			var teamRowBinVal = homeTeamBinVal.slice(15);
-			var homeTeamRowNum = await bin2Dec(teamRowBinVal);
+			var homeTeamRowNum = await FranchiseUtils.bin2Dec(teamRowBinVal);
 			
 			if(homeTeamRowNum === twoSeedTeamRows[0] || homeTeamRowNum === twoSeedTeamRows[1])
 			{
@@ -111,8 +58,8 @@ franchise.on('ready', async function () {
 		}
     }
 	
-    await franchise.save();
-    console.log("Playoff format updated successfully. Enter anything to exit the program.");
+	console.log("\nPlayoff format updated successfully.\n");
+    await FranchiseUtils.saveFranchiseFile(franchise);
     prompt();
   
 });
