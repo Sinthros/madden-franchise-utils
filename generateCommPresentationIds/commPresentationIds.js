2b9afff70afbef6aae7aed4cf29ad907e50c7f0a
noahjele <noahjele@gmail.com>
2024-02-08 01:15:05 -0600

    Commentary/Presentation ID Script

---
+++ generateCommPresentationIds/commPresentationIds.js
@@ -0,0 +1,75 @@
+// Required modules
+const prompt = require('prompt-sync')();
+const Franchise = require('madden-franchise');
+const fs = require('fs');
+const FranchiseUtils = require('../lookupFunctions/FranchiseUtils');
+const { tables } = require('../lookupFunctions/FranchiseTableId');
+const commentaryLookup = JSON.parse(fs.readFileSync('../lookupFunctions/JsonLookups/commentary_lookup.json', 'utf8'));
+const presentationIdLookup = JSON.parse(fs.readFileSync('presentationIdLookup.json', 'utf8'));
+
+// Print tool header message
+console.log("This program will generate Presentation IDs/Commentary IDs/Asset Names for Draft Class players. This is only for Madden 24 Franchise Files.")
+
+const gameYear = '24';
+const franchise = FranchiseUtils.selectFranchiseFile(gameYear);
+
+function removeSuffixes(name) {
+    return name.replace(/\s+(Jr\.?|Sr\.?|III|II|IV|V)$/g, '');
+}
+
+franchise.on('ready', async function () {
+    const playerTable = franchise.getTableByUniqueId(tables.playerTable);
+    await playerTable.readRecords();
+	
+	// Number of rows in the player table
+    const numRows = playerTable.header.recordCapacity; 
+    var currentPresentationId = presentationIdLookup['PresentationId']; //Change this value in the json if needed
+	
+	// Iterate through the player table
+    for (i = 0; i < numRows; i++) 
+	{ 
+        // If it's an empty row or invalid player, skip this row
+		if (playerTable.records[i].isEmpty || playerTable.records[i]['ContractStatus'] !== 'Draft')
+		{
+			continue;
+        }
+        const firstName = playerTable.records[i]['FirstName'];
+        const lastName = playerTable.records[i]['LastName'];
+        let commentId;
+
+        // Remove specified characters from the first name and last name
+        const cleanedFirstName = firstName.replace(/[.'`\- ]/g, '');
+        const cleanedLastName = lastName.replace(/[.'`\- ]/g, '');
+        playerTable.records[i]['PresentationId'] = currentPresentationId;
+        playerTable.records[i]['PLYR_ASSETNAME'] = `${cleanedLastName}${cleanedFirstName}_${currentPresentationId}`;
+        // Check if the exact last name exists in the commentaryLookup
+        if (commentaryLookup.hasOwnProperty(lastName)) {
+            commentId = commentaryLookup[lastName];
+        } else {
+            // Exact match not found, try to find a match after removing suffixes
+            const lastNameWithoutSuffix = removeSuffixes(lastName);
+            if (commentaryLookup.hasOwnProperty(lastNameWithoutSuffix)) {
+                commentId = commentaryLookup[lastNameWithoutSuffix];
+            } else {
+                commentId = 8191;
+            }
+        }
+        playerTable.records[i]['PLYR_COMMENT'] = commentId;
+        currentPresentationId++;
+    }
+	
+    // Update the presentationIdLookup JSON with the new currentPresentationId
+    presentationIdLookup['PresentationId'] = currentPresentationId;
+    try {
+        fs.writeFileSync('presentationIdLookup.json', JSON.stringify(presentationIdLookup, null, 2), 'utf8');
+    } catch (error) {
+        console.log("Error saving presentationIdLookup.json:", error);
+    }
+
+	console.log("Generated Asset Names/Presentation IDs/Commentary IDs for all Draft Class Players.");
+    await FranchiseUtils.saveFranchiseFile(franchise);
+    console.log("Enter anything to exit the program.");
+    prompt();
+  
+});
+  
