7450593ca8aed1cf01e319f0565b39920ee44a5c
Sinthros <noah.jeleniewski@snhu.edu>
2024-04-07 01:01:39 -0500

    Update Talent Trees

--- getTalentTreeData/getTalentTreeData.js
+++ getTalentTreeData/getTalentTreeData.js
@@ -12,7 +12,7 @@ const coachTalentPositions = JSON.parse(fs.readFileSync(path.join(__dirname, '..
 const coachTalentDependencies = JSON.parse(fs.readFileSync(path.join(__dirname, 'coach_talents_dependencies.json'), 'utf8'));
 const ZERO_REF = '00000000000000000000000000000000';
 const twoUpgradeTalentTrees = ['Staff Modifications','Trades','Offense Recruitment','Defense Recruitment'];
-const threeUpgradeTalents = ['After School Tutoring'];
+const threeUpgradeTalents = ['After School Tutoring']; // We need to specficially define this talent as having 3 upgrades because it's in Staff Modifications, which has all other talents as 2 upgrade talents
 const nonUpgradableTalentTypes = ['Regular','Selection'];
 
 const gamePrompt = '24';
