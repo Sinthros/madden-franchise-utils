0d4ef387ddeb748a27999aba0b8ef1873aa80ae2
primetime02454 <150446840+primetime02454@users.noreply.github.com>
2024-11-16 07:30:31 -0500

    Update salaryCapGenerator.js
    Added support for Madden 25 and increased max random value to $25 million

--- salaryCapGenerator/salaryCapGenerator.js
+++ salaryCapGenerator/salaryCapGenerator.js
@@ -8,11 +8,12 @@ const validGameYears = [
   FranchiseUtils.YEARS.M21,
   FranchiseUtils.YEARS.M22,
   FranchiseUtils.YEARS.M23,
-  FranchiseUtils.YEARS.M24
+  FranchiseUtils.YEARS.M24,
+  FranchiseUtils.YEARS.M25
 ];
 
 // Print tool header message
-console.log("This program will randomly generate the yearly salary cap increases.");
+console.log("This program will randomly generate the yearly salary cap increases over a 30 season period.");
 
 // Get the file's game year from the user
 const gameYear = FranchiseUtils.getGameYear(validGameYears);
@@ -35,8 +36,8 @@ franchise.on('ready', async function () {
 
   // Iterate through each row of the salary cap increase array table
   for (let i = 0; i < salCapIncreaseTable.header.numMembers; i++) {
-    // Generate a random integer between 0 and 2000
-    const randomInt = FranchiseUtils.getRandomNumber(0, 2000);
+    // Generate a random integer between 0 and 2500
+    const randomInt = FranchiseUtils.getRandomNumber(0, 2500);
 
     // Update the current salary cap increase column (0th row and i-th column) with the random integer
     salCapIncreaseTable.records[0][`int${i}`] = randomInt;
