e41eb4159fa3aa241660d209c3c81944ba015793
Sinthros <noah.jeleniewski@snhu.edu>
2024-12-08 14:19:19 -0600

    Add class to search values

---
+++ updateRosterSizes/searchValues.js
@@ -0,0 +1,63 @@
+const FranchiseUtils = require('../Utils/FranchiseUtils');
+
+const validGameYears = [
+  FranchiseUtils.YEARS.M20,
+  FranchiseUtils.YEARS.M21,
+  FranchiseUtils.YEARS.M22,
+  FranchiseUtils.YEARS.M23,
+  FranchiseUtils.YEARS.M24,
+  FranchiseUtils.YEARS.M25,
+];
+
+console.log("This program allows you to search all text fields across all tables for values containing the text you supply to search.");
+
+const isFtc = FranchiseUtils.getYesOrNo("Is your file an FTC file? Enter yes or no. If you don't know what this means, enter no.");
+
+const franchise = FranchiseUtils.init(validGameYears, {promptForBackup: false, isFtcFile: isFtc});
+
+franchise.on("ready", async function () {
+  // Preload records for all tables
+  const tableData = [];
+  for (const table of franchise.tables) {
+    await table.readRecords();
+    const columns = FranchiseUtils.getColumnNames(table);
+    tableData.push({ table, columns });
+  }
+
+  let searchValue;
+
+  do {
+    // Prompt the user for a search value
+    searchValue = FranchiseUtils.getSearchValue(
+      "Enter a string to search across any text fields. The table IDs and names where it is contained will be returned."
+    ).toLowerCase();
+
+    for (const { table, columns } of tableData) {
+      for (const record of table.records) {
+        for (const column of columns) {
+          // Get field types
+          const fieldOffset = record._fields[column].offset;
+          const { enum: enumField, isReference, type, minValue, maxValue, maxLength } = fieldOffset;
+          const isEnum = enumField !== undefined;
+          
+          if (fieldOffset.type === "string" || isEnum) {
+            const cellValue = String(record[column]).toLowerCase();
+            if (cellValue.includes(searchValue)) {
+              console.log(
+                `Table ID: ${table.header.tableId}, Table Name: ${table.header.tableId}, Row: ${record.index},  Column Name: ${column}, Value: ${record[column]}`
+              );
+            }
+          }
+        }
+      }
+    }
+  } while (
+    FranchiseUtils.getYesOrNo("Would you like to search for another term? Enter yes or no.")
+  );
+
+  FranchiseUtils.EXIT_PROGRAM();
+});
+
+
+
+
