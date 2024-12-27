0a7c6b4f9ed544fa9919bc612aca46a963acb2f8
Aashu Krishnan <gpstore31@gmail.com>
2024-12-27 01:12:50 -0600

    Added interned string JSON to BIN feature

---
+++ stringInternConverter/buildExe.bat
@@ -0,0 +1,1 @@
+nexe --build -i stringInternConverter.js -t x64-14.15.3 -r "../Utils/FranchiseUtils.js" -r "../node_modules/madden-franchise/services/lookupFiles/*.json" -o "stringInternConverter.exe" --verbose 
