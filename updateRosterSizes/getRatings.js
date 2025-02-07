7acf5c5418722007b43cc99023e96fce4f8e36ba
Sinthros <noah.jeleniewski@snhu.edu>
2025-02-07 12:14:40 -0600

    Class to export ratings

---
+++ updateRosterSizes/getRatings.js
@@ -0,0 +1,50 @@
+const FranchiseUtils = require('../Utils/FranchiseUtils');
+const fs = require("fs");
+
+const validGameYears = [
+  FranchiseUtils.YEARS.M25,
+];
+
+console.log("This program will update player ratings from one franchise file into another.");
+
+const franchise = FranchiseUtils.init(validGameYears, {customYearMessage: "Select the Madden version of your SOURCE Franchise file. Valid inputs are 24 and 25.", promptForBackup: false, isAutoUnemptyEnabled: false});
+
+const TABLES = FranchiseUtils.getTablesObject(franchise);
+
+
+franchise.on('ready', async function () {
+
+  const playerTable = franchise.getTableByUniqueId(TABLES.playerTable);
+  await FranchiseUtils.readTableRecords([playerTable]);
+  const map = {}; // Use an object (dictionary) instead of an array
+  
+  const columns = FranchiseUtils.getColumnNames(playerTable);
+  
+  // Filter for columns with "Rating" in the name but not "Original", or includes "Grade"
+  const filteredColumns = columns.filter(
+    (column) => (column.includes("Rating") && !column.includes("Original") && column !== 'RunningStyleRating' && column !== 'IsAGradePlayer') || column.includes("Grade")
+  );
+  
+  for (const record of playerTable.records) {
+    if (!FranchiseUtils.isValidPlayer(record)) continue;
+  
+    const assetName = record.PLYR_ASSETNAME;
+    map[assetName] = {};
+  
+    for (const col of filteredColumns) {
+      map[assetName][col] = record[col]; // Assign values dynamically
+    }
+  }
+  
+  console.log("Successfully outputted player ratings");
+
+  // Convert the map to JSON and write to file
+  const jsonOutput = JSON.stringify(map, null, 2);
+  fs.writeFileSync("output.json", jsonOutput, "utf8");
+
+  console.log("JSON data successfully written to output.json");
+  FranchiseUtils.EXIT_PROGRAM();
+});
+
+
+
