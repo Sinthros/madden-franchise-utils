88fe59bf9de31828e8ae5bdaaf001fc052968a60
Sinthros <noah.jeleniewski@snhu.edu>
2026-03-28 12:16:04 -0500

    fix ui select

--- MFTSetup/getUISelectForm.js
+++ MFTSetup/getUISelectForm.js
@@ -6,20 +6,9 @@ const gameYear = FranchiseUtils.YEARS.M26;
 const franchise = FranchiseUtils.init(gameYear, { isFtcFile: true, promptForBackup: false });
 
 franchise.on("ready", async function () {
-  const uiFormTable = franchise.getTableByUniqueId(1715);
+  const uiFormTable = franchise.getTableById(609);
 
-  const binary = "10000000000000000011110111110000";
-
-  const result = FranchiseUtils.resolveBinaryToTableRow(franchise, binary);
-  process.exit(0)
-
-  if (!result) {
-    console.log("Binary not found in asset table");
-  } else {
-  }
-
-  const table = franchise.getTableById(515);
-  await FranchiseUtils.readTableRecords([table]);
-  const tableArray = await FranchiseUtils.getTableDataAsArray(franchise, table);
-  FranchiseUtils.convertArrayToJSONFile(tableArray, "515 - EnumTable.json");
+  await FranchiseUtils.readTableRecords([uiFormTable]);
+  const tableArray = await FranchiseUtils.getTableDataAsArray(franchise, uiFormTable);
+  FranchiseUtils.convertArrayToJSONFile(tableArray, "UISelectForm.json");
 });
